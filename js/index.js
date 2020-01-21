c045f6e2ffc8e826cb9640606bc2601c081549a0
LonEdit120 <30282410+LonEdit120@users.noreply.github.com>
2020-01-21 21:44:48 +0900

    warmup for 1/21
    space in the middle unsolved (padding cannon set to 0)

---
+++ js/index.js
@@ -0,0 +1,7 @@
+jQuery(document).ready( () => {
+    //let update_storage = () => {console.log("updated!");}
+
+    $("update_btn").click(function(event) {
+        //console.log("updated!");
+    });
+});
